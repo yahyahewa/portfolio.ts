91641b126cc03cc10336406ff62f1ab515d32627
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-09-02 15:00:50 +0300

    config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
-    </>
+    <div className="bg-blue-500 p-4">
+      <h1 className="text-2xl text-white">Hello, Tailwind CSS!</h1>
+    </div>
   )
 }
 
