d9b8e06285857ab0f51b055a22e45fea4f69f22e
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-09-04 02:59:56 +0300

    chnage logic

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -15,14 +15,27 @@ const initialFormData: UserState = {
 };
 
 function Projects() {
+  const [characterCount, setCharacterCount] = useState<number>(0);
   const [formData, setFormData] = useState<UserState>(initialFormData);
+  
   const handleData = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const inputText = e.target.value;
+    const inputLength = inputText.length;
+    
+    // Limit the character count to 300
+    if (inputLength <= 300 && e.target.name.toString() == "message") {
+      setFormData({ ...formData, [e.target.name]: inputText });
+      setCharacterCount(inputLength);
+    }
+    else{
+      setFormData({ ...formData, [e.target.name]: inputText });
+    }
   };
   
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setFormData({ name: "", email: "", message: "" });
+    setCharacterCount(0)
   };
   return (
     <section id="contact" className="pt-40 font-sans min-h-screen">
@@ -36,7 +49,8 @@ function Projects() {
          <div className="w-14 h-4 rounded-full bg-jaguar-400
           shadow-md"></div>
           </div>
-          <form onSubmit={handleSubmit} className="w-full md:w-3/4 lg:h-2/3 xl:w-1/2 p-10 flex flex-col items-center justify-center gap-y-10">
+          <form onSubmit={handleSubmit} className="w-full md:w-3/4 lg:h-2/3 xl:w-1/2 p-10 
+          flex flex-col items-center justify-center gap-y-10 shadow-xl bg-neutral-50 rounded-lg">
             <div className="grid gap-y-5 w-full">
               <label className="text-neutral-500 text-xl font-bold ml-2">Name</label>
               <input 
@@ -65,6 +79,10 @@ function Projects() {
               onChange={handleData}
               value={formData.message}
               className="w-full bg-neutral-200 p-5 text-xl outline-none rounded-md font-bold text-neutral-500 h-48" placeholder="Enter Your Message" ></textarea>
+              <p className={characterCount > 300 ? 'text-red-500' : 'text-neutral-500'}>
+        Character: {characterCount}/300
+        {characterCount > 300 && <span> - Exceeded character limit!</span>}
+      </p>
             </div>
             <div className="grid gap-y-5 w-full"> 
               <button
