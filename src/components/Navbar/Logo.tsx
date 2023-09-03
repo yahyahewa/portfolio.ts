22433beddc5678c77cb4cd3287fa82023f2541c0
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-09-03 14:47:44 +0300

    change logo

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -1,8 +1,9 @@
 import {Link} from "react-scroll"
+import logo from "../../assets/images/Green (1).png"
 function Logo() {
   return (
-    <Link to="/" className={`flex items-center gap-x-2`}>
-        <img src={"https://yahya-hiwa.netlify.app/assets/viber-4b89879f.jpg"} 
+    <Link to="home" className={`flex items-center gap-x-2`}>
+        <img src={logo} 
         className="rounded-full
          w-8 md:w-11 p-[1px] border-jaguar-400 border-2"/>
         <h1 className="md:text-base font-medium lg:text-xl 
